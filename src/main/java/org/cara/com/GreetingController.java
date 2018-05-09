83a82bea4a314a9a54216a4b6b4845d09edde707
Cara Zhou <cara.zhou@uilanetworks.com>
2018-05-09 23:17:47 +0800

    精准推送到自己的browser

--- src/main/java/org/cara/com/GreetingController.java
+++ src/main/java/org/cara/com/GreetingController.java
@@ -1,17 +1,39 @@
 package org.cara.com;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class GreetingController {
 	
+	@Autowired
+    private SimpMessagingTemplate template;
+	
+//	@MessageMapping("/hello")
+//	@SendTo("/topic/greetings")
+//	public Greeting greeting(HelloMessage message) throws InterruptedException {
+//		Thread.sleep(1000); //simulated delay
+//		return new Greeting("Hello, " + message.getName());
+//	}
+	
 	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws InterruptedException {
+	@SendToUser(value = "/topic/greetings", broadcast = false)
+	public Greeting greeting(HelloMessage message, Principal principal) throws InterruptedException {
 		Thread.sleep(1000); //simulated delay
 		return new Greeting("Hello, " + message.getName());
-	}
+	}	
+	
+	@RequestMapping(value = "/simMessage")
+    public String greeting() throws Exception {
+        template.convertAndSend("/topic/greetings",
+                new Greeting("Hello, Other!"));
+        return "sample";
+    }
 }
